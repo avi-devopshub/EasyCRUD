b1152f917b0f2e8aaa18654ac4d2f9cbde40a3fb
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-26 14:18:15 +0530

    Update Terraform S3 backend region from ap-south-2 to ap-south-1

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket = "avidevops.fun"
     key    = "eks/terraform.tfstate"
-    region = "ap-south-2"
+    region = "ap-south-1"
   }
 
   required_version = ">= 1.3"
